d069d03bc224b34d351c13d48401b70d0ce7a130
Shalunoff-m <Shalunoff-m@yandex.ru>
2022-10-29 19:02:59 +0300

    feat: add module files

--- webpack.config.js
+++ webpack.config.js
@@ -22,19 +22,17 @@ module.exports = {
 
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        use: "babel-loader",
-        exclude: "/node_modules/",
-      },
+      // {
+      //   test: /\.js$/,
+      //   use: "babel-loader",
+      //   exclude: "/node_modules/",
+      // },
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: "asset/resource",
       },
       {
         test: /\.css$/,
-        // при обработке этих файлов нужно использовать
-        // MiniCssExtractPlugin.loader и css-loader
         use: [
           MiniCssExtractPlugin.loader,
           {
