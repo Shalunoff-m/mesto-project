12a1db227333763090c682cf57cb6d5b67a85b98
Shalunoff-m <Shalunoff-m@yandex.ru>
2022-11-04 14:17:55 +0300

    feat: часть работы над api

---
+++ src/components/api.js
@@ -0,0 +1,29 @@
+const token = "269e9438-b227-400f-84a0-3e13cb6c82c5";
+const group = "plus-cohort-16";
+const address = "https://mesto.nomoreparties.co";
+const profile = "users/me";
+export const api = {
+  token,
+  group,
+  get,
+};
+
+function get() {
+  return fetch(`${address}/v1/${group}/${profile}`, {
+    headers: {
+      authorization: token,
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Что-то пошло не так, ответа нет: ${res.status}`);
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
