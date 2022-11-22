e0ecb7a3b71480709985093450104fff9a475290
Shalunoff-m <Shalunoff-m@yandex.ru>
2022-11-22 09:35:52 +0300

    refact: + класс api с методами

--- src/components/api.js
+++ src/components/api.js
@@ -1,95 +1,96 @@
-const token = "269e9438-b227-400f-84a0-3e13cb6c82c5";
-const group = "plus-cohort-16";
-const address = "https://nomoreparties.co";
-const profile = "users/me";
-const cards = "cards";
-const like = "likes";
-const avatar = "avatar";
-
-const config = {
-  baseUrl: `${address}/v1/${group}/`,
-  headers: {
-    authorization: token,
+export class Api {
+  static token = "269e9438-b227-400f-84a0-3e13cb6c82c5";
+  static group = "plus-cohort-16";
+  static address = "https://nomoreparties.co";
+  static headers = {
+    authorization: this.token,
     "Content-Type": "application/json",
-  },
-};
+  };
+  static baseUrl = `${this.address}/v1/${this.group}/`;
 
-export const api = {
-  token,
-  group,
-  cards,
-  profile,
-  getServerData,
-  saveUserProfile,
-  saveNewCard,
-  deleteCard,
-  addLike,
-  removeLike,
-  saveAvatar,
-};
+  constructor() {}
 
-function getServerData(opt) {
-  return fetch(`${config.baseUrl}${opt}`, {
-    headers: config.headers,
-  }).then(checkResponce);
-}
-
-function deleteCard(id) {
-  return fetch(`${config.baseUrl}${cards}/${id}`, {
-    method: "DELETE",
-    headers: config.headers,
-  }).then(checkResponce);
-}
-
-function addLike(id) {
-  return fetch(`${config.baseUrl}${cards}/${like}/${id}`, {
-    method: "PUT",
-    headers: config.headers,
-  }).then(checkResponce);
-}
-
-function removeLike(id) {
-  return fetch(`${config.baseUrl}${cards}/${like}/${id}`, {
-    method: "DELETE",
-    headers: config.headers,
-  }).then(checkResponce);
-}
-
-function saveUserProfile(dataForm) {
-  return fetch(`${config.baseUrl}${profile}`, {
-    method: "PATCH",
-    headers: config.headers,
-    body: JSON.stringify({
-      name: dataForm.name,
-      about: dataForm.job,
-    }),
-  }).then(checkResponce);
-}
-
-function saveNewCard(dataForm) {
-  return fetch(`${config.baseUrl}${cards}`, {
-    method: "POST",
-    headers: config.headers,
-    body: JSON.stringify({
-      name: dataForm.name,
-      link: dataForm.link,
-    }),
-  }).then(checkResponce);
-}
-
-function saveAvatar(data) {
-  return fetch(`${config.baseUrl}${profile}/${avatar}`, {
-    method: "PATCH",
-    headers: config.headers,
-    body: JSON.stringify({
-      avatar: data.link,
-    }),
-  }).then(checkResponce);
-}
-
-function checkResponce(res) {
-  if (res.ok) {
-    return res.json();
+  // Публичный метод получения данных профиля
+  getProfileData() {
+    return fetch(`${this.constructor.baseUrl}users/me`, {
+      headers: this.constructor.headers,
+    }).then(this._checkResponce);
   }
-  return Promise.reject(`Ошибка: ${res.status}`);
+
+  // Получение всех карточек с сервера
+  getCards() {
+    return fetch(`${this.constructor.baseUrl}cards`, {
+      headers: this.constructor.headers,
+    }).then(this._checkResponce);
+  }
+
+  // Удаление карточки с сервера
+  deleteCard(id) {
+    return fetch(`${this.constructor.baseUrl}cards/${id}`, {
+      method: "DELETE",
+      headers: this.constructor.headers,
+    }).then(this._checkResponce);
+  }
+
+  // Установка лайка
+  addLike(id) {
+    return fetch(`${this.constructor.baseUrl}cards/like/${id}`, {
+      method: "PUT",
+      headers: this.constructor.headers,
+    }).then(this._checkResponce);
+  }
+
+  // Удаление лайка
+  removeLike(id) {
+    return fetch(`${this.constructor.baseUrl}cards/like/${id}`, {
+      method: "DELETE",
+      headers: this.constructor.headers,
+    }).then(this._checkResponce);
+  }
+
+  saveUserdata(data) {
+    // console.log(data);
+    return fetch(`${this.constructor.baseUrl}users/me`, {
+      method: "PATCH",
+      headers: this.constructor.headers,
+      body: JSON.stringify({
+        name: data.name,
+        about: data.job,
+      }),
+    }).then(this._checkResponce);
+  }
+
+  saveNewCard(data) {
+    return fetch(`${this.constructor.baseUrl}cards`, {
+      method: "POST",
+      headers: this.constructor.headers,
+      body: JSON.stringify({
+        name: data.name,
+        link: data.link,
+      }),
+    }).then(this._checkResponce);
+  }
+
+  saveAvatar(data) {
+    return fetch(`${this.constructor.baseUrl}users/me/avatar`, {
+      method: "PATCH",
+      headers: this.constructor.headers,
+      body: JSON.stringify({
+        avatar: data.link,
+      }),
+    }).then(this._checkResponce);
+  }
+
+  // Просмотреть полученные данные в консоли
+  showData(data) {
+    console.log(data);
+  }
+
+  // Внутренний обработчик возвращаемых данных
+  _checkResponce(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,32 +4,17 @@ import "./index.css";
 //  ----------------------------------
 // импорт js модулей
 // BM js/ глобальное подключение модулей
-
+import { Api } from "./../components/api";
 //  ----------------------------------
 // Основной код
 
-class SomeClass {
-  static _firstNumber = 55;
-  _text2 = "Какой-то текст";
-
-  // static configure(settings) {
-  //   this._valueOne = settings.value;
-  //   // console.log(this._valueOne);
-  // }
-
-  constructor(opt) {
-    this._someNumber = this.constructor._firstNumber;
-    this._text = this._text2;
-    this.val = opt.value;
-  }
-
-  showVal() {
-    console.log(this._someNumber);
-    console.log(this._text2);
-    console.log(this.val);
-  }
-}
-
-// SomeClass.configure({ value: 100 });
-const inst = new SomeClass({ value: 100 });
-inst.showVal();
+// BM js/ Основной js код
+const api = new Api();
+api
+  .getCards()
+  .then((data) => {
+    api.showData(data);
+  })
+  .catch((errData) => {
+    api.showData(errData);
+  });
